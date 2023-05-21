11c0f3a9598b38e8d31e25f8defbe0c2fc619d35
Ramesh <ramesh-km@outlook.com>
2023-05-21 22:37:36 +0530

    fix: azure redis cli connection issue

--- api/src/common/redis-client.ts
+++ api/src/common/redis-client.ts
@@ -2,7 +2,8 @@ import { createClient } from "redis";
 import { getConfig } from "./config";
 
 const redisClient = createClient({
-  url: getConfig("REDIS_URL"),
+  url: `rediss://${getConfig("REDIS_HOST")}`,
+  password: getConfig("REDIS_PASSWORD") as string,
 });
 
 export default redisClient;
